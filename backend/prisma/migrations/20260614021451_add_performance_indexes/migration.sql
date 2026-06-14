d06cabfa0638a5a77d03db2aeefbe7e22bc599f4
Araragi <davegonzales.cbsd@gmail.com>
2026-06-14 10:22:36 +0800

    chore: add IF NOT EXISTS clauses to post and user performance indexes

--- backend/prisma/migrations/20260614021451_add_performance_indexes/migration.sql
+++ backend/prisma/migrations/20260614021451_add_performance_indexes/migration.sql
@@ -1,5 +1,5 @@
 -- CreateIndex
-CREATE INDEX "posts_like_count_save_count_created_at_idx" ON "posts"("like_count", "save_count", "created_at");
+CREATE INDEX IF NOT EXISTS "posts_like_count_save_count_created_at_idx" ON "posts"("like_count", "save_count", "created_at");
 
 -- CreateIndex
-CREATE INDEX "users_follower_count_created_at_idx" ON "users"("follower_count", "created_at");
+CREATE INDEX IF NOT EXISTS "users_follower_count_created_at_idx" ON "users"("follower_count", "created_at");
